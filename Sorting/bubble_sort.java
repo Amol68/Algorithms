fe03c3cf54ed32d8d9d819168cbbd135a64f2546
Amol68 <109221958+Amol68@users.noreply.github.com>
2023-08-20 00:00:14 +0530

    bubble sort implementation
    bubble sort implementation

--- Sorting/bubble_sort.java
+++ Sorting/bubble_sort.java
@@ -33,3 +33,44 @@ public class bubble_sort {
 	}
 
 }
+
+
+
+------------->------------->------------->------------->------------->------------->------------->------------->------------->
+
+
+	package Sorting;
+
+public class bubble_sort {
+
+	public static void main(String[] args) {
+		
+		String[] arr = {"BAA" , "CAA", "AAA", "ZAA" };
+		String temp;
+		
+		for(int i=0; i<arr.length; i++) {  
+			int flag=0;
+			for(int j=0; j<arr.length-1-i; j++) {
+				
+				if(arr[j].compareTo(arr[j+1])>0) {
+				  temp = arr[j];
+				  arr[j]=arr[j+1];
+				  arr[j+1]=temp;
+				  flag=1;
+				}
+				
+			}
+			if(flag==0) {
+				break;
+			}
+			
+			
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+		  System.out.println(arr[i] +" ");
+		}
+
+	}
+
+}

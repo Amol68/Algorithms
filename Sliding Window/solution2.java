c2db46a12f7bce73fb43022095949d47bddd5000
Amol68 <109221958+Amol68@users.noreply.github.com>
2025-01-28 11:25:22 +0530

    First negative in every window of size k

--- Sliding Window/solution2.java
+++ Sliding Window/solution2.java
@@ -1,17 +1,17 @@
 // Description: https://www.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1
 
 {/* 
-  1. We're creating a list to store all the negative elements of a current window. At a particular point of time, 
-  the list holds negative numbers which are there in the current running window and not all the negative elements in the array.
+  1. We're creating a queue to store all the negative elements of a current window. At a particular point of time, 
+  the queue holds negative numbers which are there in the current running window and not all the negative elements in the array.
   So, that we can retrieve first negative number from the current window.
   
 2. When we reached the window size, we need to perform two operations:-
 
 -> First, we have to retrieve the answer that is the first negative number from the current window .
-   We're checking if the list is empty or not. 
-   If it is empty, then there is no negative number in the current window. In that case we'll push 0 in the vector.
-   If it's not empty, then the first element in the list is the first negative number of the current window., 
-   pushing that into the vector.
+   We're checking if the queue is empty or not. 
+   If it is empty, then there is no negative number in the current window. In that case we'll push 0 in the result list.
+   If it's not empty, then the first element in the queue is the first negative number of the current window., 
+   pushing that into the result list.
    
 -> Second, we need to slide the window. For that we need to remove the first element of the current window, 
     and add the next element from  the array to the window. But before removing the first element, 

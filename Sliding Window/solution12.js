2dfefa468dfdda1499795fcafadafe7465c940c0
Amol <109221958+Amol68@users.noreply.github.com>
2026-05-19 23:51:09 +0530

    Longest Substring Without Repeating Characters

---
+++ Sliding Window/solution12.js
@@ -0,0 +1,39 @@
+// problem: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+
+var lengthOfLongestSubstring = function (s) {
+
+    if(!s || !s.length || s==="") return 0;
+    if(s === " ") return 1;
+
+    if(s.length===1) return 1;
+
+    let hset = new Set();
+    let n = s.length;
+    let maxLength = -1;
+
+    for (let i = 0; i < n - 1; i++) {
+
+        hset.clear();
+        hset.add(s.charAt([i]));
+        maxLength = Math.max(maxLength, 1);
+
+        for (let j = i + 1; j < n; j++) {
+
+
+            if (hset.has(s[j])) {
+                break;
+            }
+            else {
+                hset.add(s.charAt(j));
+                maxLength = Math.max(maxLength, (j - i) + 1);
+            }
+        }
+    }
+  return maxLength;
+};
+
+// Time Complexity : O(N^2)
+// Space Complexity : O(256)
+
+// ----------->----------->--------- Better Approach --------->------>------>------>------>------>------>------>

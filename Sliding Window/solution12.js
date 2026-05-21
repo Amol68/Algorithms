5f9deca702a217271580a878a8c3c9420299bd19
Amol <109221958+Amol68@users.noreply.github.com>
2026-05-21 16:48:23 +0530

    Longest Substring Without Repeating Characters

--- Sliding Window/solution12.js
+++ Sliding Window/solution12.js
@@ -37,3 +37,39 @@ var lengthOfLongestSubstring = function (s) {
 // Space Complexity : O(256)
 
 // ----------->----------->--------- Better Approach --------->------>------>------>------>------>------>------>
+
+
+var lengthOfLongestSubstring = function (s) {
+
+    if (!s || !s.length || s === "") return 0;
+    if (s === " ") return 1;
+
+    if (s.length === 1) return 1;
+
+    let hmap = new Map();
+    let n = s.length;
+    let maxLength = -1;
+    let l = 0;
+
+    for (let r = 0; r < n; r++) {
+
+        let char = s.charAt(r);
+
+        hmap.set(char, (hmap.get(char) || 0) + 1);
+
+        while (hmap.get(char) > 1) {
+            let leftChar = s.charAt(l)
+
+            hmap.set(leftChar, hmap.get(leftChar) - 1);
+            l++;
+        }
+
+        maxLength = Math.max(maxLength, (r - l) + 1);
+
+    }
+return maxLength;
+}
+
+
+// Time Complexity : O(N)
+// Space Complexity : O(1)
